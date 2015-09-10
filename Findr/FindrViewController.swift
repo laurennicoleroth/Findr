6bce7ebb468806a430247932444d8defd86bdba2
Lauren Nicole Roth <laurenroth@gmail.com>
2015-09-10 16:39:47 -0400

    Add in initial cards.

--- Findr/FindrViewController.swift
+++ Findr/FindrViewController.swift
@@ -14,39 +14,21 @@ class FindrViewController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    
-    func addPerson(url: String, gender: String, preference: String, fullname: String) {
-        var newUser = PFUser() as PFUser!
-        var imgURL: NSURL = NSURL(string: url)!
+
+        PFGeoPoint.geoPointForCurrentLocationInBackground {
+            (geoPoint: PFGeoPoint?, error: NSError?) -> Void in
+            if error == nil {
+                var user = PFUser.currentUser() as PFUser!
+                user["location"] = geoPoint
+                user.saveInBackground()
+            } else {
+                println(error)
+            }
+        }
         
-        let request: NSURLRequest = NSURLRequest(URL: imgURL)
-        NSURLConnection.sendAsynchronousRequest(
-            request, queue: NSOperationQueue.mainQueue(),
-            completionHandler: {(response: NSURLResponse!,data: NSData!,error: NSError!) -> Void in
-                if error == nil {
-                    var image = UIImage(data: data)
-                    var file = PFFile(data: UIImageJPEGRepresentation(image, 1.0))
-                    newUser["picture"] = file
-                    newUser["gender"] = gender
-                    newUser["preference"] = preference
-                    newUser["fullname"] = fullname
-                    var lat = Double(Int(arc4random_uniform(100)+1))
-                    var lon = Double(Int(arc4random_uniform(100)+1))
-                    var location = PFGeoPoint(latitude: lat, longitude: lon)
-                    newUser["location"] = location
-                    var name = "\(fullname)" + String(Int(arc4random_uniform(100)+1))
-                    let username = name.stringByReplacingOccurrencesOfString(" ", withString: "").lowercaseString
-                    newUser.username = username
-                    newUser.password = "password"
-                    newUser.signUp()
-                } else {
-                    println(error)
-                }
-        })
-       
+        loadPositiveSingle()
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -63,6 +45,10 @@ class FindrViewController: UIViewController {
         positiveSingle.addGestureRecognizer(gesture)
         
         positiveSingle.userInteractionEnabled = true
+        
+        var userImage: UIImageView = UIImageView(frame: CGRectMake(self.view.bounds.width / 2 - 100, self.view.bounds.height / 2 - 50, 200, 100))
+        userImage.image = UIImage(named: "placeholder-avatar.png")
+        self.view.addSubview(userImage)
     }
     
     func wasDragged(gesture: UIPanGestureRecognizer) {
